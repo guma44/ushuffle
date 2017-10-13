6ef234fe3a24baa6d101ac09d852116480974b75
Rafal Gumienny <r.gumienny@unibas.ch>
2017-10-13 16:44:58 +0200

    chore: Update setup

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup, Extension
 from Cython.Build import cythonize
 
-ext_type = Extension("ushuffle",
-                     sources=["ushuffle.pyx",
+ext_type = Extension("py_ushuffle",
+                     sources=["py_ushuffle.pyx",
                               "src/ushuffle.c"])
 
-setup(name="ushuffle",
+setup(name="py_ushuffle",
       version="1.0",
       ext_modules=cythonize([ext_type]))
