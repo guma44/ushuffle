e74321f583974f399744b8fdc4f02c4778effb0b
Rafal Gumienny <r.gumienny@unibas.ch>
2017-10-16 10:30:37 +0200

    feat: Add accessible attributes and meaningfull names

--- py_ushuffle.pyx
+++ py_ushuffle.pyx
@@ -1,10 +1,10 @@
 cimport ushuffle_mod
 from libc.stdlib cimport malloc, free
 
-def shuffle(char *s, int k):
+def shuffle(char *s, int let_size):
     cdef int length = len(s)
     cdef char *t = <char *>malloc((len(s)) * sizeof(char))
-    ushuffle_mod.shuffle(s, t, length, k)
+    ushuffle_mod.shuffle(s, t, length, let_size)
     try:
         py_t = t[:length]
     finally:
@@ -14,23 +14,28 @@ def shuffle(char *s, int k):
 cdef class Shuffler:
     cdef char *s
     cdef char *t
-    cdef int length
-    cdef int k
+    cdef readonly int length
+    cdef readonly int let_size
 
-    def __cinit__(self, char *s, int l, int k):
+    def __cinit__(self, char *s, int length, int let_size):
         self.s = s
-        self.k = k
-        self.length = l
+        self.let_size = let_size
+        self.length = length
         self.t = <char *>malloc(self.length * sizeof(char))
-        ushuffle_mod.shuffle1(self.s, self.length, self.k)
+        ushuffle_mod.shuffle1(self.s, self.length, self.let_size)
 
     def shuffle(self):
-        # print "data: ", self.s, self.length, self.k
+        # print "data: ", self.s, self.length, self.let_size
         # print "before:", self.t
         ushuffle_mod.shuffle2(self.t)
         # print "after:", self.t
         py_t = self.t[:self.length]
         return py_t
+
+    property sequence:
+        def __get__(self):
+            return self.s[:self.length]
+
 
     def __dealloc__(self):
         free(<void *>self.t)
