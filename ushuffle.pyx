01abc2f1c2e15b35c386420aa54849b41d3156bd
Rafal Gumienny <guma44@gmail.com>
2017-10-16 23:55:41 +0200

    docs: Update docstring

--- ushuffle.pyx
+++ ushuffle.pyx
@@ -2,6 +2,8 @@ cimport ushuffle_mod
 from libc.stdlib cimport malloc, free
 
 def shuffle(char *sequence, int let_size):
+    """Shuffle the sequence while preserving k-let size.
+    """
     cdef int length = len(s)
     cdef char *t = <char *>malloc((len(s)) * sizeof(char))
     ushuffle_mod.shuffle(sequence, t, length, let_size)
@@ -12,23 +14,21 @@ def shuffle(char *sequence, int let_size):
     return py_t
 
 cdef class Shuffler:
+    """A sequence shuffler with k-let size preservation."""
     cdef char *s
     cdef char *t
     cdef readonly int length
     cdef readonly int let_size
 
-    def __cinit__(self, char *s, int length, int let_size):
-        self.s = s
+    def __cinit__(self, char *sequence, int length, int let_size):
+        self.s = sequence
         self.let_size = let_size
         self.length = length
         self.t = <char *>malloc(self.length * sizeof(char))
         ushuffle_mod.shuffle1(self.s, self.length, self.let_size)
 
     def shuffle(self):
-        # print "data: ", self.s, self.length, self.let_size
-        # print "before:", self.t
         ushuffle_mod.shuffle2(self.t)
-        # print "after:", self.t
         py_t = self.t[:self.length]
         return py_t
 
